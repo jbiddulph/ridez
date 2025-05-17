582994e40e17597492f2b470369486e6d0e0ccf1
John <john.mbiddulph@gmail.com>
2025-05-17 10:56:20 +0100

    added AppTrackingTrancparency

--- app.vue
+++ app.vue
@@ -3,3 +3,24 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  document.addEventListener('deviceready', function() {
+    document.getElementById('requestTrackingBtn').addEventListener('click', function() {
+      if (window.plugins && window.plugins.idfa) {
+        window.plugins.idfa.requestPermission(function(result) {
+          if (result === 'authorized') {
+            console.log('Tracking authorized');
+          } else {
+            console.log('Tracking not authorized:', result);
+          }
+        });
+      }
+    });
+  }, false);
+});
+</script>
+
